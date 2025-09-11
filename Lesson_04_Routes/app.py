912a4791de040be921a8e69b65e8fba1be9a1fed
sophie <soplee27@bergen.org>
2025-09-11 10:26:56 -0400

    end of class 9/11

---
+++ Lesson_04_Routes/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+<h1>Dynamic Route Demo</h1>
+<h2>Try: These URLS</h2>
+<ul>
+    <li><a href="/user/john"></a>User Profile: john</li>
+    <li><a href="/user/alice"></a>User Profile: alice</li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
+"""
+
+@app.route('/user/<username>', methods=['GET'])
+def user_profile(username):
+    return """
+    <!-- User Profile -->
+<h1>User profile</h1>
+<p>Username: <strong>{username}</strong></p>
+<p>Profile Type: {type(username).__name__}</p>
+<p>Welcome to {username}'s profile page!</p>
+<nav>
+    <a href="/">Back to Homepage</a>
+    <a href="/user/alice">Alice</a>
+    <a href="/user/bob">Bob</a>
+</nav>
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
