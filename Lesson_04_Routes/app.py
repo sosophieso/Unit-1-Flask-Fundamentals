46c035a940bb4303af58711d36335215d7bd0744
sophie <soplee27@bergen.org>
2025-09-15 10:24:39 -0400

    calc done l4

--- Lesson_04_Routes/app.py
+++ Lesson_04_Routes/app.py
@@ -2,36 +2,48 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return """
-<h1>Dynamic Route Demo</h1>
+           <h1>Dynamic Routes Demo</h1>
 <h2>Try: These URLS</h2>
 <ul>
-    <li><a href="/user/john"></a>User Profile: john</li>
-    <li><a href="/user/alice"></a>User Profile: alice</li>
+    <li><a href="/user/john">User Profile: john</a></li>
+    <li><a href="/user/alice">User Profile: alice</a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
-</ul>
+</ul> 
 """
 
-@app.route('/user/<username>', methods=['GET'])
+@app.route('/user/<username>')
 def user_profile(username):
-    return """
-    <!-- User Profile -->
-<h1>User profile</h1>
-<p>Username: <strong>{username}</strong></p>
-<p>Profile Type: {type(username).__name__}</p>
-<p>Welcome to {username}'s profile page!</p>
-<nav>
-    <a href="/">Back to Homepage</a>
+    return f'''
+     <h1>User Profile</h1>
+ <p>Username: <strong>{username}</strong></p>
+ <p>Profile Type: {type(username).__name__}</p>
+ <p>Welcome to {username}'s profile page!</p>
+ <nav>
+    <a href="/"> Back to Homepage</a>
     <a href="/user/alice">Alice</a>
     <a href="/user/bob">Bob</a>
-</nav>
-"""
+ </nav>    
+'''
+@app.route("/calc/<int:num1>/<operation>/<int:num2>")
+def calculator(num1, operation, num2):
+    operations = {
+        '+' : num1 + num2,
+        '-' : num1 - num2,
+        '*' : num1 * num2,
+        '/' : num1 / num2 if num2 != 0 else 'Error: Division by zero!'
+    }
+    if operation in operations:
+        result = operations[operation]
+        return f"{num1} {operation} {num2} = {result}"
+    else:
+        return f"Unknown operation!{operation}"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
